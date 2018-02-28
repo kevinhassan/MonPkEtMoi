7b3686a2ef10948988408dfa003d856850971291
Yves-Alain AGBODJOGBE <yves-alain.agbodjogb@igtp4-mac04.isim.intra>
2018-02-28 11:20:00 +0100

    Add some views

--- monPkEtMoi/ViewController.swift
+++ monPkEtMoi/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    var names : [String] = []
+    @IBOutlet weak var contactMedic: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return self.names.count
+    
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = self.contactMedic.dequeueReusableCell(withIdentifier: "contactMedicalCell",for: indexPath) as! ContactMedicTableViewCell
+        cell.prenomMedic.text = self.names[indexPath.row]
+        return cell
+    
+    }
+    
 }
 
