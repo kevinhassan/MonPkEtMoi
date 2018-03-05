cc5b6fdf5ab2a152cbf30f6b18017cb40a4a5d28
Yves-Alain AGBODJOGBE <yves-alain.agbodjogb@igtp3-mac05.isim.intra>
2018-03-05 17:42:24 +0100

    add :controller

---
+++ monPkEtMoi/patientModel.swift
@@ -0,0 +1,27 @@
+//
+//  patientModel.swift
+//  monPkEtMoi
+//
+//  Created by Yves-Alain AGBODJOGBE on 05/03/2018.
+//  Copyright © 2018 CHU-polytech. All rights reserved.
+//
+
+import CoreData
+import UIKit
+import Foundation
+
+/**
+ Person type
+ 
+ **nom**: Person -> String
+ **prenom**: Person -> String
+ **nomcomplet**: Person -> String
+ **birthDate**: Person -> NSDate?
+ **age**: Person -> Int?
+ */
+
+
+extension Patient {
+    
+}
+
---
+++ monPkEtMoi/personModel.swift
@@ -0,0 +1,9 @@
+//
+//  personModel.swift
+//  monPkEtMoi
+//
+//  Created by Yves-Alain AGBODJOGBE on 05/03/2018.
+//  Copyright © 2018 CHU-polytech. All rights reserved.
+//
+
+import Foundation
