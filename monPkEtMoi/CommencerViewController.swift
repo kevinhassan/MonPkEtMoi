104d31e79393a62a5bd083ffee5d6a52a8a30cbc
Yves-Alain AGBODJOGBE <yves-alain.agbodjogb@igtp4-mac08.isim.intra>
2018-03-07 17:50:49 +0100

    add: savefunction with text field

--- monPkEtMoi/CommencerViewController.swift
+++ monPkEtMoi/CommencerViewController.swift
@@ -14,48 +14,43 @@ class CommencerViewController: UIViewController, UITableViewDelegate{
     
     var patients : [Patient] = []
     
+    @IBOutlet weak var boutonCommencer: UIButton!
     
-    @IBAction func addAction(_ sener: Any){
-        let alert = UIAlertController(title: "Nouveau Nom",
-                                      message: "Ajouter un nom",
-                                      preferredStyle: .alert)
-        
-        let saveAction = UIAlertAction(title : "Ajouter",
-                                       style: .default)
-        
-        {
-            [unowned self] action in
-            guard let textField = alert.textFields?.first,
-                let nameToSave = textField.text else{
-                    return
-            }
-            self.saveNewPatient(withName: nameToSave)
-    
-        }
-        
-        let cancelAction = UIAlertAction(title: "Annuler",
-                                         style: .default)
-        
-        alert.addTextField()
-        alert.addAction(saveAction)
-        alert.addAction(cancelAction)
-        
-        present(alert,animated: true)
-    }
-    
+//    @IBAction func addAction(_ sender: Any){
+//        let alert = UIAlertController(title: "Nouveau Nom",
+//                                      message: "Ajouter un nom",
+//                                      preferredStyle: .alert)
+//        
+//        
+//        let cancelAction = UIAlertAction(title: "Annuler",
+//                                         style: .default)
+//        
+//        alert.addTextField()
+//        alert.addAction(cancelAction)
+//        
+//        present(alert,animated: true)
+//    }
+//    
    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            self.alerteError(errorMsg : "Could not save person", userinfo: "Unknow reason")
-            return
-        }
-        
-        let context = appDelegate.persistentContainer.viewContext
-        // creer une requête associée à l'entité patient
-                
+//        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+//            //self.alerteError(errorMsg : "Could not save person", userinfo: "Unknow reason")
+//            return
+//        }
+//        
+//        let context = appDelegate.persistentContainer.viewContext
+//        // creer une requête associée à l'entité patient
+//        let request : NSFetchRequest<Patient> = Patient.fetchRequest()
+//        
+//        do{
+//            try self.patients = context.fetch(request)
+//        }catch let error as NSError{
+            //self.alerteError(errorMsg: "\(error)", userinfo: "\(error.userInfo)")
+            
+//        }
         
     }
     
@@ -66,42 +61,22 @@ class CommencerViewController: UIViewController, UITableViewDelegate{
     }
     
     
-    
-    func saveNewPatient(withName nom: String){
-        //first get context into application delegate
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            self.alerteError(errorMsg : "Could not save person", userinfo: "Unknow reason")
-            return
-        }
-        
-        let context = appDelegate.persistentContainer.viewContext
-        //Create a person managedObject
-        let patient = Patient(context : context)
-        //then modify its name
-        patient.nom = nom
-        do{
-            try context.save()
-            self.patients.append(patient)
-        }catch let error as NSError{
-            self.alerteError(errorMsg: "\(error)", userinfo: "\(error.userInfo)")
-            return
-        }
-    }
-
-    
-    func alerteError(errorMsg error : String, userinfo user : String = "") {
-        let alert = UIAlertController(title: error,
-                                      message: user,
-                                      preferredStyle: .alert)
-        
-        let cancelAction = UIAlertAction(title: "Ok",
-                                         style: .default)
-
-        
-        alert.addAction(cancelAction)
-        present(alert,animated: true)
-
-    }
+//    
+//    
+//    
+//    func alerteError(errorMsg error : String, userinfo user : String = "") {
+//        let alert = UIAlertController(title: error,
+//                                      message: user,
+//                                      preferredStyle: .alert)
+//        
+//        let cancelAction = UIAlertAction(title: "Ok",
+//                                         style: .default)
+//
+//        
+//        alert.addAction(cancelAction)
+//        present(alert,animated: true)
+//
+//    }
     
     
     /*
--- monPkEtMoi/ViewController.swift
+++ monPkEtMoi/ViewController.swift
@@ -9,8 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var contactMedic: UITableView!
-    override func viewDidLoad() {
+        override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
