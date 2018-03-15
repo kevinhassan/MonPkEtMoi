67964d3b28de3395d7a5d4f88bc1cfda8ff828c5
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2018-03-15 17:09:44 +0100

    edit: views prises

---
+++ monPkEtMoi/Controllers/ShowEventPatientViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ShowEventPatientTableViewController.swift
+//  monPkEtMoi
+//
+//  Created by Kevin HASSAN on 15/03/2018.
+//  Copyright © 2018 CHU-polytech. All rights reserved.
+//
+
+import UIKit
+
+class ShowEventPatientViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
