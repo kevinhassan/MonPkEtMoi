205d570e986c4dcfe4722186cbc96c77d144cf35
Kevin HASSAN <kevin.hassan@igtp4-mac07.isim.intra>
2018-03-15 12:59:30 +0100

    add: controller to show patient events

---
+++ monPkEtMoi/Controllers/ShowEventPatientTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ShowEventPatientTableViewController.swift
+//  monPkEtMoi
+//
+//  Created by Kevin HASSAN on 15/03/2018.
+//  Copyright © 2018 CHU-polytech. All rights reserved.
+//
+
+import UIKit
+
+class ShowEventPatientTableViewController: UITableViewController {
+
+    var events: [Evenement]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
--- monPkEtMoi/DAO/EvenementDAOExtension.swift
+++ monPkEtMoi/DAO/EvenementDAOExtension.swift
@@ -11,4 +11,13 @@ import CoreData
 import UIKit
 
 extension Evenement{
+    static func getAll() throws -> [Evenement]{
+        let request: NSFetchRequest<Evenement> = Evenement.fetchRequest()
+        do {
+            let events: [Evenement] = try CoreDataManager.context.fetch(request)
+            return events
+        } catch let error as NSError {
+            throw error
+        }
+    }
 }
