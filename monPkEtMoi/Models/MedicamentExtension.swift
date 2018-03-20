90fa8e490f5d658ec4bf582fd4d0ae69b79ba6fb
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2018-03-20 14:17:04 -0700

    add: medicament extension

--- monPkEtMoi/Models/MedicamentExtension.swift
+++ monPkEtMoi/Models/MedicamentExtension.swift
@@ -5,57 +5,55 @@
 //  Created by macOS on 20/03/2018.
 //  Copyright © 2018 CHU-polytech. All rights reserved.
 //
+import Foundation
+import CoreData
 
 extension Medicament{
-    static func create(withAdresse: String, withMail: String, withNom: String, withPrenom: String, withTel: String, withTempsPreparation: Int64, withDateNaissance: NSDate) throws -> Patient {
+    static func create(withDescription: String?, withNom: String, withDosage: [String]?) throws -> Medicament {
         
-        let patient = Patient(context: CoreDataManager.context)
+        let medicament = Medicament(context: CoreDataManager.context)
         
-        patient.adresse = withAdresse
-        patient.mail = withMail
-        patient.nom = withNom
-        patient.prenom = withPrenom
-        patient.tel = withTel
-        patient.tempsPreparation = withTempsPreparation
-        patient.dateNaissance = withDateNaissance
+        medicament.descriptionMedicament = withDescription
+        medicament.nomMedicament = withNom
+        medicament.dosageMedicament = withDosage
+        
         do{
             try CoreDataManager.save()
         }catch let error as NSError{
             throw error
         }
-        return patient
+        return medicament
     }
     
-    func edit(withAdresse: String, withMail: String, withNom: String, withPrenom: String, withTel: String, withTempsPreparation: Int64, withDateNaissance: NSDate) {
-        self.adresse = withAdresse
-        self.mail = withMail
-        self.nom = withNom
-        self.prenom = withPrenom
-        self.tel = withTel
-        self.tempsPreparation = withTempsPreparation
-        self.dateNaissance = withDateNaissance
+    func edit(withDescription: String?, withNom: String?, withDosage: [String]?) {
+        if let description = withDescription{
+            self.descriptionMedicament = description
+        }
+        if let nom = withNom{
+            self.nomMedicament = nom
+        }
+        if let dosage = withDosage{
+            self.dosageMedicament = dosage
+        }
     }
-    static func get() throws -> Patient? {
-        let request: NSFetchRequest<Patient> = Patient.fetchRequest()
+    static func get(withNom: String) throws -> Medicament? {
+        let predicate = NSPredicate(format: "nomMedicament == %@", withNom)
+        let request: NSFetchRequest<Medicament> = Medicament.fetchRequest()
+        request.predicate = predicate
         do {
-            let patients: [Patient] = try CoreDataManager.context.fetch(request)
-            if (patients.count > 0) {
-                return patients[0]
-            } else {
-                return nil
-            }
+            let medicament: Medicament? = try CoreDataManager.context.fetch(request).first
+            return medicament
         } catch let error as NSError {
             throw error
         }
     }
-    static func exists() throws -> Bool {
-        do {
-            guard let _: Patient = try self.get() else {
-                return nil
-            }
-            return true
-        } catch let error as NSError {
+    func delete() throws {
+        do{
+            CoreDataManager.context.delete(self)
+            try CoreDataManager.save()
+        }catch let error as NSError{
             throw error
         }
     }
+    
 }
